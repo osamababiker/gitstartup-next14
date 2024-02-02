9e15799cc76929826eca797c90a5710850ce1500
osama babiker <u.osamababiker@gmail.com>
2024-02-02 17:44:26 +0400

    change the video image in  the home page

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,7 +1,7 @@
 module.exports = [
   {
     id: 1,
-    imgSrc: "/images/gallery/img_00.png",
+    imgSrc: "/images/gallery/2.png",
     videoUrl: "zQl46QTLPsw",
     iconClass: "bi bi-play-fill",
     overlayClass: "video-icon overlay-icon",
