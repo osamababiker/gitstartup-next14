9cf2c9f945901e13f4aafc1e2ad051f9f071b507
osama babiker <u.osamababiker@gmail.com>
2024-02-07 13:04:46 +0400

    change our video at home page

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -2,7 +2,7 @@ module.exports = [
   {
     id: 1,
     imgSrc: "/images/gallery/img_00.png",
-    videoUrl: "zQl46QTLPsw",
+    videoUrl: "anYjy7kh6Mo",
     iconClass: "bi bi-play-fill",
     overlayClass: "video-icon overlay-icon",
     captionTags: "Our",
