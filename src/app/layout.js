cf18509119b9fe22162fd2a969888ed56b6a0c91
osama babiker <u.osamababiker@gmail.com>
2024-02-08 17:57:13 +0400

    initialize hotjar services

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from 'next/font/google'
 import BootstrapClient from '@/components/common/BootstrapClient'
 import ScrollTop from '@/components/common/ScrollTop'
+import Hotjar from '@hotjar/browser'
 import './globals.scss'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -9,7 +10,12 @@ export const metadata = {
   title: 'Git Startup',
   description: 'technical support for your business idea',
 } 
- 
+
+// to initialize hotjar services
+const siteId = 3858905
+const hotjarVersion = 6
+
+Hotjar.init(siteId, hotjarVersion)
 
 export default function RootLayout({ children }) {
   return (
